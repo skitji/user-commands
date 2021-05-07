2ef6da0c3d536533a0eeeabdeb393645934409c0
skitji <josch@skitji.de>
2021-05-07 20:12:20 +0200

    opened for more commands and parameters

--- UserChannelCreator.js
+++ UserChannelCreator.js
@@ -27,44 +27,75 @@ registerPlugin({
 	var engine = require('engine');
 	var backend = require('backend');
 	var event = require('event');
-	var channels = [];
-	var mainChannels = [];
+	var userChannels = [];
+	var userHeaderChannels = [];
 	var parentIndex;
 	
 	event.on('chat', function(ev) {
-		var msg = ev.text;
-		var cmdArgs = msg.split(' ');
 		if(ev.mode == 1) {
-			if(cmdArgs[0] == '!channel' && cmdArgs.length == 2) {
-				createUserChannel(ev.client, cmdArgs[1]);
-			} else {
-				ev.client.chat('The command is !channel <password>')
-			}
+			main(ev.text, ev.client);
 		}
 		
 	});
-	
-	function createUserChannel(client, pw) {
-		if(channels.length == 0) {
-			mainChannels.push(backend.createChannel({name: '[*spacer998]___', parent: 0, permanent: true}));
-			mainChannels.push(backend.createChannel({name: '[cspacer] User Channels', parent: 0, permanent: true}));
-			parentIndex = 1;
-			mainChannels.push(backend.createChannel({name: '[*spacer999]___', parent: 0, permanent: true}));
+
+	const main = (command, sender) => {
+		let args = new Map();
+		let cmd = command.split("-")[0].replace('!', '').trim();
+		command.trim()
+				.split("-")
+				.slice(1,command.split("-").length)
+				.map(x => x.trim().split(" "))
+				.forEach(x => {
+					engine.log("x" + x);
+					args.set(x[0], x[1]);
+				});
+		for([key, value] of args) {
+			if(key == 'p' && value == undefined) break;
+			if(value == undefined) args.set(key, true);
 		}
-		var channelName = config.name.replace('%c', client.name());
-		var create = backend.createChannel({parent: mainChannels[parentIndex].id(), name: channelName, password: pw});
+		
+		engine.log(args.get('p'));
+		engine.log(args.get('dm'));
+		engine.log(cmd + "|");
+		switch (cmd) {
+			case 'channel':
+				return createUserChannel(sender, args);
+			default:
+				break;
+		}
+	}
+	
+	const createUserChannel = (sender, args) => {
+		createUserHeaderChannels();
+		var channelName = config.name.replace('%c', sender.name());
+		var create = backend.createChannel({parent: userHeaderChannels[parentIndex].id(), name: channelName});
+		if(args.get('p') != undefined) {
+			create.setPassword(args.get('p'))
+		}
 		perm = create.addPermission("i_icon_id");
 		perm.setValue(config.IconId);
 		perm.save();
-		channels.push(create.id());
-		client.moveTo(create);
+		userChannels.push(create.id());
+		if(!args.get('dm')) {
+			sender.moveTo(create);
+		}
+		
+	}
+
+	const createUserHeaderChannels = () => {
+		if(userChannels.length == 0) {
+			userHeaderChannels.push(backend.createChannel({name: '[*spacer998]___', parent: 0, permanent: true}));
+			userHeaderChannels.push(backend.createChannel({name: '[cspacer] User Channels', parent: 0, permanent: true}));
+			parentIndex = 1;
+			userHeaderChannels.push(backend.createChannel({name: '[*spacer999]___', parent: 0, permanent: true}));
+		}
 	}
 	
 	event.on('channelDelete', function(ev) {
-		channels = channels.filter(x => x != ev.id());
-		if(channels.length == 0) {
-			mainChannels.forEach(x => x.delete());
-			mainChannels = [];
+		userChannels = userChannels.filter(x => x != ev.id());
+		if(userChannels.length == 0) {
+			userHeaderChannels.forEach(x => x.delete());
+			userHeaderChannels = [];
 		}
 	});
 })
