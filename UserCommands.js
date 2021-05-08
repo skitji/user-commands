b116bcb9e714fda94601dfe3ee35c8b6ed40b78a
skitji <josch@skitji.de>
2021-05-08 13:09:07 +0200

    Adds help command and  aliases for commands

--- UserCommands.js
+++ UserCommands.js
@@ -3,41 +3,57 @@ registerPlugin({
 	version: '1.0.0',
 	backends: ['ts3'],
 	engine: '>= 0.13.37',
-    description: 'An easy way to add commands',
-    author: 'skitji <info@skitji.de>',
+	description: 'An easy way to add commands',
+	author: 'skitji <info@skitji.de>',
 	vars: [{
-		name: 'GroupChannel',
-		indent: 1,
-		title: 'Group channel',
-		type: 'channel'
-	},
-	{
-		name: 'IconId',
-		ident: 1,
-		title: 'Icon ID',
-		type: 'number'
-	}, 
-	{
-		name: 'name',
-		ident: 1,
-		title: 'Channel Name (%c for client)',
-		type: 'string'
-	}]
-}, function(sinusbot, config) {
+			name: 'GroupChannel',
+			indent: 1,
+			title: 'Group channel',
+			type: 'channel'
+		},
+		{
+			name: 'IconId',
+			ident: 1,
+			title: 'Icon ID',
+			type: 'number'
+		},
+		{
+			name: 'name',
+			ident: 1,
+			title: 'Channel Name (%c for client)',
+			type: 'string'
+		}
+	]
+}, function (sinusbot, config) {
 	const engine = require('engine');
 	const backend = require('backend');
 	const event = require('event');
-	const helpMessages = {
-		'channel': '-p set a password \n -dm don\'t move' 
+	const commandInfo = {
+		'channel': {
+			'Description': 'Creates your own channel',
+			'Aliases': ['c', 'createchannel'],
+			'-p': 'Sets a password',
+			'-dm': 'If you don\'t want to be move'
+		},
+		'help': {
+			'Description': 'Get all commands',
+			'Aliases': ['commands', 'h'],
+		}
 	}
 	var userChannels = [];
 	var userHeaderChannels = [];
 	var parentIndex;
 	
-	event.on('chat', function(ev) {
-		if(ev.mode == 1) {
+
+	event.on('chat', function (ev) {
+		if (ev.mode == 1) {
 			main(ev.text, ev.client);
 		}
+	});
+
+	event.on('channelDelete', function (ev) {
+		userChannels = userChannels.filter(x => x != ev.id());
+		if (userChannels.length == 0) deleteUserChannelsHeader();
 	});
 
 	/**
@@ -48,68 +64,124 @@ registerPlugin({
 	const main = (command, sender) => {
 		let args = new Map();
 		let splittedCommand = command.trim().split("-");
-		let cmd = splittedCommand[0].replace('!', '').trim();
+		let cmd = getCommand(splittedCommand[0].replace('!', '').trim());
 		splittedCommand
-				.slice(1,splittedCommand.length)
-				.map(x => x.trim().split(" "))
-				.forEach(x => {
-					args.set(x[0], x[1]);
-				});
-		for([key, value] of args) {
-			if(key == 'h') sender.chat('Help for !' + cmd + '\n' + helpMessages[cmd]);
-			if(key == 'p' && value == undefined) break;
-			if(value == undefined) args.set(key, true);
+			.slice(1, splittedCommand.length)
+			.map(x => x.trim().split(" "))
+			.forEach(x => {
+				args.set(x[0], x[1]);
+			});
+		
+		for ([key, value] of args) {
+			if (key == 'h') {
+				let text = '';
+				text += '!' + cmd + ':\n';
+				for(k in commandInfo[cmd]) {
+					text += k + '\t' + commandInfo[cmd][k] + '\n';
+				}
+				sender.chat(text);
+				return;
+			}
+			if (key == 'p' && value == undefined) break;
+			if (value == undefined) args.set(key, true);
 		}
 		switch (cmd) {
 			case 'channel':
-				return createUserChannel(sender, args);
+				createUserChannel(sender, args);
+				break;
+			case 'help':
+				sendCommands(sender, args);
+				break;
 			default:
+				sendCommands(sender, args);
 				break;
 		}
 	}
-	
+
+	/**
+	 * Checks alias and returns command
+	 * @param {string} command command or alias
+	 * @returns command
+	 */
+	const getCommand = (command) => {
+		for(prop in commandInfo) {
+			for(alias of commandInfo[prop]['Aliases']) {
+				if(command == alias) {
+					return prop;
+				}
+			}
+		}
+		return command;
+	}
+
 	/**
 	 * Creates a channel for the sender
 	 * @param {Client} sender client who send command
 	 * @param {Map<string,string>} args arguments for command
 	 */
 	const createUserChannel = (sender, args) => {
-		if(userChannels.length == 0) createUserChannelsHeader();
+		if (userChannels.length == 0) createUserChannelsHeader();
 		var channelName = config.name.replace('%c', sender.name());
-		var create = backend.createChannel({parent: userHeaderChannels[parentIndex].id(), name: channelName});
-		if(args.get('p') != undefined) {
+		var create = backend.createChannel({
+			parent: userHeaderChannels[parentIndex].id(),
+			name: channelName
+		});
+		if (args.get('p') != undefined) {
 			create.setPassword(args.get('p'))
 		}
 		perm = create.addPermission("i_icon_id");
 		perm.setValue(config.IconId);
 		perm.save();
 		userChannels.push(create.id());
-		if(!args.get('dm')) {
+		if (!args.get('dm')) {
 			sender.moveTo(create);
 		}
-		
+
 	}
 
 	/**
 	 * Creates the header for all user channels
 	 */
 	const createUserChannelsHeader = () => {
-		userHeaderChannels.push(backend.createChannel({name: '[*spacer998]___', parent: 0, permanent: true}));
-		userHeaderChannels.push(backend.createChannel({name: '[cspacer] User Channels', parent: 0, permanent: true}));
+		userHeaderChannels.push(backend.createChannel({
+			name: '[*spacer998]___',
+			parent: 0,
+			permanent: true
+		}));
+		userHeaderChannels.push(backend.createChannel({
+			name: '[cspacer] User Channels',
+			parent: 0,
+			permanent: true
+		}));
 		parentIndex = 1;
-		userHeaderChannels.push(backend.createChannel({name: '[*spacer999]___', parent: 0, permanent: true}));
+		userHeaderChannels.push(backend.createChannel({
+			name: '[*spacer999]___',
+			parent: 0,
+			permanent: true
+		}));
 	}
 
 	/**
 	 * Deletes the header for all user channels
 	 */
-	const deleteUserChannelsHeader = () => {	
+	const deleteUserChannelsHeader = () => {
 		userHeaderChannels.forEach(x => x.delete());
 		userHeaderChannels = []
 	}
-	
-	event.on('channelDelete', function(ev) {
-		userChannels = userChannels.filter(x => x != ev.id());
-		if(userChannels.length == 0) deleteUserChannelsHeader();
-	});
+
+	/**
+	 * Sends a help message
+	 * @param {Client} sender client who send command
+	 * @param {Map<string,string>} args arguments for command
+	 */
+	const sendCommands = (sender, args) => {
+		let text = '';
+		for(key in commandInfo) {
+			text += '!' + key + ':\n';
+			for(k in commandInfo[key]) {
+				text += k + '\t' + commandInfo[key][k] + '\n';
+			}
+		}
+		sender.chat(text);
+	}
 })
